809d994a41e95c271a8d1f58b209c32c464f66b5
wangye349 <wangy349@mail2.sysu.edu.cn>
2019-11-06 20:44:48 +0800

    add some asserts
    Signed-off-by: wangye349 <wangy349@mail2.sysu.edu.cn>

--- preprocessing.py
+++ preprocessing.py
@@ -7,11 +7,15 @@ class StandardScaler():
         self.scale_ = None
 
     def fit(self, X):
-        self.mean_ = [np.mean(X[:,i]) for i in X.shape[1]]
-        self.scale_ = [np.std(X[:,i]) for i in X.shape[1]]
+        assert X.ndim == 2, "the shape of X must be 2"
+        self.mean_ = np.array([np.mean(X[:,i]) for i in X.shape[1]])
+        self.scale_ = np.array([np.std(X[:,i]) for i in X.shape[1]])
         return self
 
     def transform(self, X):
+        assert X.ndim == 2, "the shape of X must be 2"
+        assert self.mean_ != None and self.scale_ != None, "the array must be fitted before transform"
+        assert self.mean_.shape[1] == self.scale_.shape[1] == X.shape[1], "the shape of X must be the same as its mean adn std"
         X_temp = np.empty(shape=X.shape, dtype=float);
         for i in range(X.shape[1]):
             X_temp[:,i] = (X[:,i] - self.mean_[i]) / self.scale_[i]
